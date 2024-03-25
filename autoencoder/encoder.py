2344e6c1dc44e13b0bf007275b7800b101e1b5b1
wisebreadloaf <kpprasanna24@gmail.com>
2024-03-26 02:02:04 +0530

    modified files

--- autoencoder/encoder.py
+++ autoencoder/encoder.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 
-# transform = transforms.ToTensor()
 transform = transforms.Compose(
     [
         transforms.ToTensor(),
         transforms.Resize((64, 64)),
     ]
 )
+
 
 anime_data = datasets.MNIST(
     root="./data",
@@ -23,38 +23,38 @@ data_loader = torch.utils.data.DataLoader(
 )
 
 
-class Autoencoder(nn.Module):
+class ConvAutoencoder(nn.Module):
     def __init__(self):
-        super(Autoencoder, self).__init__()
+        super(ConvAutoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(64 * 64, 128),
+            nn.Conv2d(1, 16, 3, stride=2, padding=1),
             nn.ReLU(),
-            nn.Linear(128, 64),
+            nn.Conv2d(16, 32, 3, stride=2, padding=1),
             nn.ReLU(),
-            nn.Linear(64, 12),
+            nn.Conv2d(32, 64, 3, stride=2, padding=1),
             nn.ReLU(),
-            nn.Linear(12, 3),
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
+            nn.ReLU(),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(3, 12),
+            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
-            nn.Linear(12, 64),
+            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
-            nn.Linear(64, 128),
+            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
-            nn.Linear(128, 64 * 64),
+            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1),
             nn.Sigmoid(),
         )
 
     def forward(self, x):
-        x = x.view(x.size(0), -1)
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = Autoencoder().to(device)
+model = ConvAutoencoder().to(device)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-3)
 
@@ -62,14 +62,12 @@ num_epochs = 320
 for epoch in range(num_epochs):
     for img, _ in data_loader:
         img = img.to(device)
-        img = img.view(img.size(0), -1)
         recon = model(img)
         loss = criterion(recon, img)
-
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
     print(f"Epoch: {epoch+1}, Loss: {loss.item():.4f}")
 
-torch.save(model.state_dict(), "./models/linear-auto-encoder.pth")
+torch.save(model.state_dict(), "./models/conv-auto-encoder.pth")
